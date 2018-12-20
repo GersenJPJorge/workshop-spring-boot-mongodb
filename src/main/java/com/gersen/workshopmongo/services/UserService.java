5c55cbe00221f66269a4af598e709981bb7e5230
Gersen Jorge <gersen.jorge@gmail.com>
2018-12-20 12:56:54 -0200

    Obtendo usuário por Id

--- src/main/java/com/gersen/workshopmongo/services/UserService.java
+++ src/main/java/com/gersen/workshopmongo/services/UserService.java
@@ -1,25 +1,37 @@
 package com.gersen.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gersen.workshopmongo.domain.User;
 import com.gersen.workshopmongo.repository.UserRepository;
+import com.gersen.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User>  findAll () {
-		
-		return repo.findAll();
-		
-		
-	}
 
-	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+	  
+/*		public User findById(String id) {
+			User user = repo.findOne(id);
+			if (user == null ) {
+				throw new ObjectNotFoundException("Object not found");
+				
+			}
+			return user;
+		}
+*/		
+		  
+		public User findById(String id) {
+			Optional<User> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto " + id + " não encontrado "));
+		}
 }
