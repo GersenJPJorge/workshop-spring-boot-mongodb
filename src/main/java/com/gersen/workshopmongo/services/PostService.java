0f01cb7273dc99bac84654662a1576171d0f7af6
Gersen Jorge <gersen.jorge@gmail.com>
2018-12-21 18:55:36 -0200

    Consulta simples com query methods

--- src/main/java/com/gersen/workshopmongo/services/PostService.java
+++ src/main/java/com/gersen/workshopmongo/services/PostService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gersen.workshopmongo.domain.Post;
-import com.gersen.workshopmongo.domain.User;
 import com.gersen.workshopmongo.repository.PostRepository;
 import com.gersen.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -32,4 +31,9 @@ public class PostService {
 			Optional<Post> obj = repo.findById(id);
 			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto " + id + " não encontrado "));
 		}	
+		
+		public List<Post> findByTitle(String text) {
+			return repo.findByTitleContainingIgnoreCase	(text);
+		}
+		
 	}
